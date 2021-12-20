747d91da17044f0c30aa7f7411654f6e73652be4
Thomas Weller <ThomasWeller@wellisolutions.de>
2021-12-20 22:09:41 +0100

    Describe remarks
    Add 7 tests and 2 assertions to support the documentation

--- source/tests/IniFileFormatTests/Tests/Casing_Tests.cs
+++ source/tests/IniFileFormatTests/Tests/Casing_Tests.cs
@@ -11,9 +11,10 @@ namespace IniFileFormatTests
     [TestClass]
     public class Casing_Tests : IniFileTestBase
     {
-        // TODO: Review
+        [DoNotRename("Used in documentation")]
+        [TestsApiParameter("lpAppName")]
         [TestMethod]
-        public void Given_AnIniFileWithKnownContent_When_TheContentIsAccessedWithDifferentSectionCasing_Then_WeGetTheExpectedValue()
+        public void Given_AnSectionWithUpperCaseLetters_When_TheContentIsAccessedWithLowerCase_Then_WeGetTheExpectedValue()
         {
             EnsureDefaultContent_UsingFile();
             var sb = DefaultStringBuilder();
@@ -22,17 +23,27 @@ namespace IniFileFormatTests
             var bytes = GetIniString_SB_Unicode(sectionname.ToLower(), keyname, null, sb, (uint)sb.Capacity, FileName);
             AssertASCIILength(inivalue, bytes);
             AssertSbEqual(inivalue, sb);
+
+            bytes = GetIniString_SB_Unicode(sectionname.ToUpper(), keyname, null, sb, (uint)sb.Capacity, FileName);
+            AssertASCIILength(inivalue, bytes);
+            AssertSbEqual(inivalue, sb);
+
         }
 
-        // TODO: Review
+        [DoNotRename("Used in documentation")]
+        [TestsApiParameter("lpKeyName")]
         [TestMethod]
-        public void Given_AnIniFileWithKnownContent_When_TheContentIsAccessedWithDifferentKeyCasing_Then_WeGetTheExpectedValue()
+        public void Given_AnEntryWithUpperCaseLetter_When_TheContentIsAccessedWithLowerCase_Then_WeGetTheExpectedValue()
         {
             EnsureDefaultContent_UsingFile();
             var sb = DefaultStringBuilder();
 
             // Insight: key reading is case insensitive
             var bytes = GetIniString_SB_Unicode(sectionname, keyname.ToLower(), null, sb, (uint)sb.Capacity, FileName);
+            AssertASCIILength(inivalue, bytes);
+            AssertSbEqual(inivalue, sb);
+
+            bytes = GetIniString_SB_Unicode(sectionname, keyname.ToUpper(), null, sb, (uint)sb.Capacity, FileName);
             AssertASCIILength(inivalue, bytes);
             AssertSbEqual(inivalue, sb);
         }
